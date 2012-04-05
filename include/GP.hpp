9de8f66ba3e9d238a2d26517b13e030edb0501a0
Marco Halder <marco.halder@frm2.tum.de>
2012-04-05 19:45:48 +0200

    added collection header GP.hpp

---
+++ include/GP.hpp
@@ -0,0 +1,35 @@
+/* Copyright (C) 2012  Marco Halder
+ *
+ * This file is part of GP
+ * GP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GP_
+#define GP_
+
+#include "GP/Axes.hpp"
+#include "GP/BasicPlot.hpp"
+#include "GP/Binary.hpp"
+#include "GP/Clear.hpp"
+#include "GP/DataPlot.hpp"
+#include "GP/Endian.hpp"
+#include "GP/Exit.hpp"
+#include "GP/Format.hpp"
+#include "GP/FunctionPlot.hpp"
+#include "GP/History.hpp"
+#include "GP/Smooth.hpp"
+#include "GP/Stream.hpp"
+#include "GP/Style.hpp"
+#include "GP/Terminal.hpp"
+
+#endif // GP_
