93bfae0aab1ff114bf01a2bdfa011562b4d6dba8
Marco Halder <marco.halder@frm2.tum.de>
2012-04-24 08:44:36 +0200

    updated collection header GP.hpp

--- include/GP.hpp
+++ include/GP.hpp
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,10 +22,12 @@
 #include "GP/Clear.hpp"
 #include "GP/Config.hpp"
 #include "GP/DataPlot.hpp"
+#include "GP/Enum.hpp"
 #include "GP/Exit.hpp"
 #include "GP/Format.hpp"
 #include "GP/FunctionPlot.hpp"
 #include "GP/History.hpp"
+#include "GP/Set.hpp"
 #include "GP/Stream.hpp"
 
 #endif // GP_
